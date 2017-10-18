f2e446eebd51bb4f97200f432880fce08a7bc5c4
Thomas Juhnke <tommy@van-tomas.de>
2017-10-18 22:51:45 +0200

    Remove custom log file creation

--- base/rootfs/etc/entrypoint-suexec.d/app-base01.sh
+++ base/rootfs/etc/entrypoint-suexec.d/app-base01.sh
@@ -6,7 +6,3 @@ if [ -z "$SUEXEC_USER" ]; then
 fi
 
 mkdir -p -m 0770 /var/app && chown "$SUEXEC_USER":"$SUEXEC_USER" /var/app
-
-mkdir -p -m 0770 /var/log/php && chown "$SUEXEC_USER":"$SUEXEC_USER" /var/log/php
-ln -sf /dev/stderr /var/log/php/error.log && chown "$SUEXEC_USER":"$SUEXEC_USER" /var/log/php/error.log
-ln -sf /dev/stdout /var/log/php/access.log && chown "$SUEXEC_USER":"$SUEXEC_USER" /var/log/php/access.log
