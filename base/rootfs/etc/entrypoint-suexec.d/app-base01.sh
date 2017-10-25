3870298fb3ee0f00abafc01919217ba61a1f120a
Thomas Juhnke <tommy@van-tomas.de>
2017-10-25 08:44:33 +0200

    Add named pipe for log redirection
    @see https://github.com/docker-library/php/issues/207
    @see https://bugs.php.net/bug.php?id=71880

--- base/rootfs/etc/entrypoint-suexec.d/app-base01.sh
+++ base/rootfs/etc/entrypoint-suexec.d/app-base01.sh
@@ -6,3 +6,14 @@ if [ -z "$SUEXEC_USER" ]; then
 fi
 
 mkdir -p -m 0770 /var/app && chown "$SUEXEC_USER":"$SUEXEC_USER" /var/app
+
+
+# Create a named pipe and redirect anything that comes to it
+# to stderr connected to Docker.
+#
+# @see https://github.com/moby/moby/issues/6880#issuecomment-220637337
+# @see https://redmine.lighttpd.net/issues/2731#note-15
+#
+mkfifo --mode 600 /var/log/app.log
+chown "$SUEXEC_USER" /var/log/app.log
+cat <> /var/log/app.log 1>&2 &
